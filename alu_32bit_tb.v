55fdb131ef4d8bc31226fd4e58765273579bcb10
Maia Nkonabang <nkonaban@usc.edu>
2026-02-12 14:19:22 -0800

    Update fully working alu_32bit_tb.v -- Maia

--- alu_32bit_tb.v
+++ alu_32bit_tb.v
@@ -23,34 +23,64 @@ module alu_32bit_tb;
         A = 0; B = 0; Op = 0; shift = 0;
         #15 reset = 0;
         
+        // ADD
         A = 32'd15; B = 32'd10; Op = 4'b0000; shift = 0;
         #10;
         
+        // SUB
         A = 32'd15; B = 32'd10; Op = 4'b0001; shift = 0;
         #10;
         
+        // AND
         A = 32'hF0F0F0F0; B = 32'h0F0F0F0F; Op = 4'b0010; shift = 0;
         #10;
         
+        // OR
         A = 32'hAAAA0000; B = 32'h5555FFFF; Op = 4'b0011; shift = 0;
-        #20;
+        #10;
         
+        // SHIFT LEFT
         A = 32'd16; B = 32'd0; Op = 4'b0100; shift = 5'd3;
-        #20;
+        #10;
         
+        // SHIFT RIGHT
         A = 32'd128; B = 32'd0; Op = 4'b0101; shift = 5'd2;
-        #20;
+        #10;
         
+        // XNOR
         A = 32'hF0F0F0F0; B = 32'hF0F0F0F0; Op = 4'b0110; shift = 0;
         #10;
         
+        // EQUAL
         A = 32'd100; B = 32'd100; Op = 4'b0111; shift = 0;
         #10;
         
+        // LESS THAN
         A = 32'd50; B = 32'd100; Op = 4'b1000; shift = 0;
         #10;
         
+        // GREATER THAN
         A = 32'd150; B = 32'd100; Op = 4'b1001; shift = 0;
+        #10;
+        
+        // XOR
+        A = 32'hAAAAAAAA; B = 32'h55555555; Op = 4'b1010; shift = 0;
+        #10;
+        
+        // SHIFT-THEN-COMPARE returns 1
+        A = 32'd8; B = 32'd64; Op = 4'b1011; shift = 5'd3;
+        #10;
+        
+        // SHIFT-THEN-COMPARE returns 0
+        A = 32'd8; B = 32'd65; Op = 4'b1011; shift = 5'd3;
+        #10;
+        
+        // SUBSTRING COMPARE returns 1
+        A = 32'hABCD1234; B = 32'h56781234; Op = 4'b1100; shift = 0;
+        #10;
+        
+        // SUBSTRING COMPARE returns 0
+        A = 32'hABCD1234; B = 32'h567812FF; Op = 4'b1100; shift = 0;
         #10;
         
         $finish;
