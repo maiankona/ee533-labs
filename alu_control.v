896a4a132cf4c9c35e0b7f624288f9f6552e5906
Maia Nkonabang <nkonaban@usc.edu>
2026-02-19 13:07:22 -0800

    added sort specific alu ops-- Maia

--- alu_control.v
+++ alu_control.v
@@ -1,26 +1,39 @@
 module alu_control(
-    input [1:0] ALUOp,      // From main control
-    input [5:0] funct,      // Funct field from instruction
-    output reg [3:0] ALUControl  // To your ALU (matches your existing Op codes)
+    input [1:0] ALUOp,
+    input [24:21] funct,      // ARM instruction bits [24:21] (cmd field)
+    input [11:4] shamt,       // Shift amount/type
+    
+    output reg [3:0] ALUControl
 );
-
-// will update after deeper analysis of bubble sort opcodes
 
 always @(*) begin
     case (ALUOp)
-        2'b00: ALUControl = 4'b0000;  // LW/SW: always ADD
-        2'b01: ALUControl = 4'b0001;  // BEQ: always SUB
-        2'b10: begin  // R-type: use funct field
+        2'b00: ALUControl = 4'b0000;  // LDR/STR: ADD
+        
+        2'b01: ALUControl = 4'b0001;  // Branch: SUB (for condition eval)
+        
+        2'b10: begin  // Data processing: decode funct
             case (funct)
-                6'b100000: ALUControl = 4'b0000;  // ADD
-                6'b100010: ALUControl = 4'b0001;  // SUB
-                6'b100100: ALUControl = 4'b0010;  // AND
-                6'b100101: ALUControl = 4'b0011;  // OR
-                6'b101010: ALUControl = 4'b1000;  // SLT (less than)
-                6'b100110: ALUControl = 4'b1010;  // XOR
-                default:   ALUControl = 4'b0000;
+                4'b0000: ALUControl = 4'b0010;  // AND
+                4'b0001: ALUControl = 4'b0110;  // EOR (XOR)
+                4'b0010: ALUControl = 4'b0001;  // SUB
+                4'b0011: ALUControl = 4'b0001;  // RSB (reverse subtract)
+                4'b0100: ALUControl = 4'b0000;  // ADD
+                4'b0101: ALUControl = 4'b0000;  // ADC (add with carry)
+                4'b0110: ALUControl = 4'b0001;  // SBC (sub with carry)
+                4'b0111: ALUControl = 4'b0001;  // RSC
+                4'b1000: ALUControl = 4'b0010;  // TST (AND for flags)
+                4'b1001: ALUControl = 4'b0110;  // TEQ (XOR for flags)
+                4'b1010: ALUControl = 4'b0001;  // CMP (SUB for flags)
+                4'b1011: ALUControl = 4'b0000;  // CMN (ADD for flags)
+                4'b1100: ALUControl = 4'b0011;  // ORR
+                4'b1101: ALUControl = 4'b0100;  // MOV (shift/pass through)
+                4'b1110: ALUControl = 4'b0010;  // BIC (bit clear)
+                4'b1111: ALUControl = 4'b0101;  // MVN (NOT)
+                default: ALUControl = 4'b0000;
             endcase
         end
+        
         default: ALUControl = 4'b0000;
     endcase
 end
