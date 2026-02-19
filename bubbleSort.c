e024109478144383d92d64e8ab2b906ade4cff5a
Maia Nkonabang <nkonaban@usc.edu>
2026-02-18 17:07:07 -0800

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,7 +1,7 @@
 int main() {
     // simple bubble sort algorithm
-    int arr[] = {64, 34, 25, 12, 22, 11, 90}; // static or via user input?
-    int n = sizeof(arr) / sizeof(arr[0]); // will inputs just be 1D or multiple dimensions?
+    int arr[] = {323, 123, -455, 2, 98, 125, 10, 65, -56, 0}; // static or via user input?
+    int n = sizeof(arr); 
     int i, j, temp;
     for (i = 0; i < n-1; i++) {
         for (j = 0; j < n-i-1; j++) {
