848a15203e284b1d27d2781c4d3dae2161dca05b
Maia Nkonabang <nkonaban@usc.edu>
2026-02-20 18:25:42 -0800

    3 bit --> 5 bit addressing (32 registers) -- Maia

--- pipeline/registerFIle32.v
+++ pipeline/registerFIle32.v
@@ -1,9 +1,9 @@
 module registerFile32(
     input clk,
     input clr,
-    input [2:0] r1addr, // Reg1 address
-    input [2:0] r2addr, // Reg2 address
-    input [2:0] waddr,  // Write address from WB stage
+    input [4:0] r1addr, // Reg1 address
+    input [4:0] r2addr, // Reg2 address
+    input [4:0] waddr,  // Write address from WB stage
     input [31:0] wdata, // Write data from WB stage
     input wena,         // Write enable from WB stage
     output [31:0] r1data,
@@ -11,7 +11,7 @@ module registerFile32(
     );
 
     // Create an array of 8 registers, each 32 bits wide
-    reg [31:0] rf [7:0];
+    reg [31:0] rf [31:0];
     integer i;
 
     // -------- Forwarding Logic (Fixes Read-During-Write) --------
@@ -22,7 +22,7 @@ module registerFile32(
     always @(posedge clk or posedge clr) begin
         if (clr) begin
             // Reset all registers to 0
-            for (i = 0; i < 8; i = i + 1)
+            for (i = 0; i < 32; i = i + 1)
                 rf[i] <= 32'b0;
         end 
         else if (wena) begin
