a6c12005d6def90cfb915067ba86a16c793884d1
Maia Nkonabang <nkonaban@usc.edu>
2026-02-20 18:35:41 -0800

    increased regs and shifted instruction format -- Maia
    reduced immediate from 12 bit to 8 bits

--- pipeline/idecode.v
+++ pipeline/idecode.v
@@ -4,14 +4,14 @@ module decode (
     input [31:0] id_inst,           // From IF/ID bridge 
     
     // Write-back interface (feedback loop from WB stage) 
-    input [2:0]  wb_waddr,          // WReg1 from WB stage
+    input [4:0]  wb_waddr,          // WReg1 from WB stage
     input [31:0] wb_wdata,          // Data from WB stage
     input        wb_wena,           // WRegEn from WB stage
     
     // Outputs to ID/EX bridge 
     output [31:0] r1data,           // Data from Reg1
     output [31:0] r2data,           // Data from Reg2
-    output [2:0]  wreg_addr_out,    // Propagated destination address
+    output [4:0]  wreg_addr_out,    // Propagated destination address
     output        wreg_en_out,      // Propagated WRegEn
     output        wmem_en_out,      // Propagated WMemEn
 
@@ -27,18 +27,19 @@ module decode (
     // 1. Instruction Parsing (Part 3 Format) 
     wire       wmem_en = id_inst[31];
     wire       wreg_en = id_inst[30];
-    wire [2:0] reg1    = id_inst[29:27];
-    wire [2:0] reg2    = id_inst[26:24];
-    wire [2:0] wreg1   = id_inst[23:21];
+    wire [4:0] reg1    = id_inst[29:25];
+    wire [4:0] reg2    = id_inst[24:20]; 
+    wire [4:0] wreg1   = id_inst[19:15]; 
 
     // *** Added parsing fields for ALU/Immediate ***
-    wire [3:0] alu_op  = id_inst[20:17];
-    wire [4:0] shift   = id_inst[10:6];
-    wire [11:0] imm12  = id_inst[11:0];
-
-    wire branch  = id_instr[15];
-    wire brType  = id_instr[14];
-
+    wire [3:0] alu_op  = id_inst[14:11];  
+    wire       ALUSrc  = id_inst[10];     
+    wire       branch  = id_inst[9];     
+    wire       brType  = id_inst[8];  
+    wire [7:0] imm8    = id_inst[7:0]; 
+    
+    assign [4:0] shift = imm8[4:0];
+    
     // 2. Register File Instance
     registerFile32 rf_inst (
         .clk(clk),
@@ -57,8 +58,8 @@ module decode (
     assign wreg_en_out   = wreg_en;
     assign wmem_en_out   = wmem_en;
 
-    // *** Immediate generation (sign extend 12-bit immediate) ***
-    assign sign_ext_imm_out = {{20{imm12[11]}}, imm12};
+    // *** Immediate generation (sign extend 8-bit immediate) ***
+    assign sign_ext_imm_out = {{24{imm8[7]}}, imm8};
 
     // *** ALU control + shift ***
     assign shift_out    = shift;
