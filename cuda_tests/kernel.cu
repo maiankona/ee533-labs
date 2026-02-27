b8ed828dd979ccbc79417ce070824729bf89234b
Maia Nkonabang <nkonaban@usc.edu>
2026-02-27 00:38:58 -0800

    simplified kernel.cu -- Maia

--- cuda_tests/kernel.cu
+++ cuda_tests/kernel.cu
@@ -1,9 +1,4 @@
 #include <cuda_bf16.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <cublas.h>
-#include <time.h>
-#include <stdint.h>
 
 // only using 1 thread so how to handle thread IDx?
 // Vector Add 
@@ -14,28 +9,28 @@ __global__ void vector_add(int16_t *a, int16_t *b, int16_t *c){
 
 // Vector Sub
 __global__ void vector_sub(int16_t *a, int16_t *b, int16_t *c){
-	int16_t idx = threadIdx.x;
+	int idx = threadIdx.x;
 	c[idx] = a[idx] - b[idx];  
 }
 
 // Bfloat 16 Multiply
 __global__ void bf16_vector_mul(__nv_bfloat16 *a, __nv_bfloat16 *b, __nv_bfloat16 *c)
 {
-  int16_t idx = threadIdx.x;
+  int idx = threadIdx.x;
   c[idx] = __hmul(a[idx], b[idx]);
 }
 
 // Bfloat 16 MAC
 __global__ void bf16_fma(__nv_bfloat16 *a, __nv_bfloat16 *b, __nv_bfloat16 *c, __nv_bfloat16 *d)
 {
-  int16_t idx = threadIdx.x;
+  int idx = threadIdx.x;
   d[idx] = __hfma(a[idx], b[idx], c[idx]);
 }
 
 
-// ReLU
+// ReLU aka setp/selp in ptx
 __global__ void ReLU(int16_t *a, int16_t *out)
 {
-  int16_t idx = threadIdx.x;
-  out[idx] = max(0, a[idx]);
+  int idx = threadIdx.x;
+  out[idx] = a[idx] > 0 ? a[idx]: 0;
 }
